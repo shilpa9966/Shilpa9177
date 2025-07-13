e85615021d39ca2472349c25aacf4e28cad70fe6
shilpa9966 <kuppannagarishilpa7130@gmail.com>
2025-07-13 11:28:46 +0530

    Add files via upload

---
+++ MCA/Sems/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render
+
+# Create your views here.
+def abc(request):
+    return render(request,'abc.html')
+def mca101(request):
+    return render(request,'mca101.html')    
+def mca102(request):
+    return render(request,'mca102.html')
+def mca103(request):
+    return render(request,'mca103.html')
+def mca104(request):
+    return render(request,'mca104.html')
+def mca105(request):
+    return render(request,'mca105.html')
+def mca106(request):
+    return render(request,'mca106.html')
+def mca107(request):
+    return render(request,'mca107.html')
+def mca108(request):
+    return render(request,'mca108.html')
+def mca201(request):
+    return render(request,'mca201.html') 
+def mca202(request):
+    return render(request,'mca202.html')  
+def mca203(request):
+    return render(request,'mca203.html')
+def mca204(request):
+    return render(request,'mca204.html')  
+def mca205(request):
+    return render(request,'mca205.html') 
+def mca206(request):
+    return render(request,'mca206.html') 
+def mca207(request):
+    return render(request,'mca207.html')    
+def mca208(request):
+    return render(request,'mca208.html')
+def mca209(request):
+    return render(request,'mca209.html')
+def mca301(request):
+    return render(request,'mca301.html')
+def mca302(request):
+    return render(request,'mca302.html')
+def mca303(request):
+    return render(request,'mca303.html') 
+def mca304(request):
+    return render(request,'mca304.html')
+def mca305(request):
+    return render(request,'mca305.html')
+def mca306(request):
+    return render(request,'mca306.html')  
+def mca307(request):
+    return render(request,'mca307.html')         
+def mca308(request):
+    return render(request,'mca308.html')  
+def mca309(request):
+    return render(request,'mca309.html')
+def mca310(request):
+    return render(request,'mca310.html')
+def mca401(request):
+    return render(request,'mca401.html')  
+def mca402(request):
+    return render(request,'mca402.html')                                                                 
+
+    
